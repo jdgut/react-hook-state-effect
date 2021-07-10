81aa7c18aec4afa0c2ab088a52f10e8b7562f60b
jdgut <jdgutierrez80@gmail.com>
2021-07-10 00:25:10 +0000

    initial demo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,49 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
+	const [inputValue, setInputValue] = useState("");
+	const [userName, setUserName] = useState("");
+	const [initialLoad, fireInitialLoad] = useState(true);
+	const [showAlert, doShowAlert] = useState(false);
+
+	useEffect(() => {
+		if (!initialLoad && inputValue === "") {
+			setUserName("");
+			doShowAlert(true);
+		} else {
+			doShowAlert(false);
+		}
+		fireInitialLoad(false);
+	}, [inputValue]);
+
+	const updateInfo = e => {
+		setInputValue(e.target.value.trim());
+	};
+
+	const validateData = () => {
+		if (inputValue === "") {
+			setUserName("");
+			doShowAlert(true);
+		} else {
+			setUserName(inputValue);
+		}
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input type="text" value={inputValue} onChange={updateInfo} />{" "}
+			&nbsp;
+			<button className="btn btn-primary" onClick={validateData}>
+				Say hello
+			</button>
+			<hr width="100%" />
+			{userName.length > 0 && <h1>Hello {userName}</h1>}
+			{showAlert && (
+				<div className="alert alert-danger" role="alert">
+					Invalid input, please fix!
+				</div>
+			)}
 		</div>
 	);
 }
